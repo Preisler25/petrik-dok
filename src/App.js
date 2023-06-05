7c39b1b68365da8904422ef2c5c2de628ac3d6ad
preisler25 <preisler.andras@gmail.com>
2023-06-05 09:46:46 +0200

    main

--- src/App.js
+++ src/App.js
@@ -6,20 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <div>
-        {generalBtn("Gomb szövege", console.log("alma"))}
+        {generalBtn("Jelentkezem", console.log("alma"))}
         </div>
       </header>
     </div>
---
+++ src/comp/textBox.js
@@ -0,0 +1,11 @@
+function generalTxt(header, txt) {
+    return (
+        <div className="txtBoxcont">
+            <div className="txtBoxHeader">{header}</div>
+            <div className="txtBoxBody">{txt}</div>
+        </div>
+      );
+};
+  
+export default generalTxt;
+  
