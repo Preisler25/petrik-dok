b7865875c41282d5c05af056e5109e8b0509118a
preisler25 <preisler.andras@gmail.com>
2023-06-05 09:50:48 +0200

    main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import generalBtn from './comp/btn';
+import generalTxt from './comp/textBox'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-        {generalBtn("Jelentkezem", console.log("alma"))}
+          {generalTxt("header", "lorem")}
+          {generalTxt("header", "lorem")}
+          {generalTxt("header", "lorem")}
+          {generalBtn("Jelentkezem", console.log("alma"))}
         </div>
       </header>
     </div>
